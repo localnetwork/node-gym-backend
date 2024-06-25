85221bf3bfae65caa4eb4c8bd421b0cf8ab41ddd
Diome Nike <diomenikep@gmail.com>
2024-06-26 00:09:20 +0800

    hotfix

--- config/db.js
+++ config/db.js
@@ -8,6 +8,14 @@ const connection = mysql.createConnection({
   password: process.env.NODE_DB_PASSWORD || "default",
   database: process.env.NODE_DB_NAME,
   port: process.env.NODE_DB_PORT || 3306,
+  // Keep alive packets should be sent
+  enableKeepAlive: true,
+  // We should start sending them early
+  keepAliveInitialDelay: 3 * 1000, // 3 seconds
+  // We don't want idle connections, but it's not mandatory for the fix to work, it seems
+  maxIdle: 0,
+  // Idle timeout much larger than keep alive delay and much smaller than MySQL's timeout setting
+  idleTimeout: 5 * 60 * 1000 // 5 minutes 
 });
 
 connection.connect((err) => {
